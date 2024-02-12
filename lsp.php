05a79d4688a072caf7a0fa677a661bd9d239e43f
Christian Schneider <schneider@search.ch>
2024-02-12 12:10:39 +0100

    Stick to plain PHP, do not use libraries

--- lsp.php
+++ lsp.php
@@ -134,7 +134,7 @@ while (!feof(STDIN))
 							['line' => $checkline, 'message' => $checkmessage] = $checkmatches;
 							$diagnostics[] = [
 								'range'   => ['start' => ['line' => $checkline - 1, 'character' => 0], 'end' => ['line' => $checkline - 1, 'character' => 0]],
-								'severity' => it::match('error', $checkmessage) ? 1 : 2,	# 1=Error, 2=Warning
+								'severity' => preg_match('/error/', $checkmessage) ? 1 : 2,	# 1=Error, 2=Warning
 								'message' => $checkmessage,
 							];
 						}
